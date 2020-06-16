7c7ec79b87e553fab457072d8d7ddf0b88003daa
范轶洁 <fanyjb@digitalchina.com>
2020-06-16 22:03:14 +0800

    pass props

--- App.vue
+++ App.vue
@@ -1,19 +1,28 @@
 <template>
   <ul class="container">
     <li v-for="item in items" :key="item.message">
-      {{ item.message }}
+      <app-message :msg="item.message"></app-message>
     </li>
   </ul>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Message from "./components/Message.vue";
 
 export default {
   data() {
     return {
       items: [{ message: "Foo" }, { message: "Bar" }],
     };
+  },
+  filters: {
+    hightlight(message) {
+      return `[${message}]`;
+    },
+  },
+  components: {
+    "app-message": Message,
   },
 };
 </script>
---
+++ components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["msg"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
